dc6b2e57c7590f2d10be25ef41947eebb3f3c279
Nakagawa-master <karyu.owner@gmail.com>
2026-06-29 07:54:43 +0900

    Add manifest freshness check

---
+++ tools/official_derivatives/check_manifest_fresh.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+
+
+def main():
+    a = subprocess.call([sys.executable, 'tools/official_derivatives/build_origin_manifest.py'])
+    if a != 0:
+        return a
+    b = subprocess.call(['git', 'diff', '--quiet', '--', 'tools/official_derivatives/origin_manifest.tsv'])
+    if b != 0:
+        print('manifest_fresh=false')
+        return b
+    print('manifest_fresh=true')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
