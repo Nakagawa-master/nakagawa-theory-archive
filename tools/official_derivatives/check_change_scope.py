86653342aaede335bd9fe0e7c3afb9155f9fcce9
Nakagawa-master <karyu.owner@gmail.com>
2026-06-29 13:38:23 +0900

    Run origin catalog validators in change scope check

--- tools/official_derivatives/check_change_scope.py
+++ tools/official_derivatives/check_change_scope.py
@@ -22,6 +22,8 @@ EXTRA_CHECKS = (
     'check_next_batch_schema.py',
     'check_next_10_queue.py',
     'check_next_10_source_inventory.py',
+    'check_next_10_origin_catalog.py',
+    'check_next_10_catalog_to_source_candidates.py',
     'check_next_10_source_candidate_schema.py',
     'check_next_10_source_quality_gate.py',
     'check_next_10_source_candidate_report.py',
@@ -75,7 +77,7 @@ def main():
             errors.append('outside_allowed_scope=' + path)
         if any(part in low for part in DISALLOWED_PARTS):
             errors.append('disallowed_path_part=' + path)
-    print('check_set=official_derivative_change_scope_v12')
+    print('check_set=official_derivative_change_scope_v13')
     print('changed_files=' + str(len(files)))
     if errors:
         print('\n'.join(errors))
