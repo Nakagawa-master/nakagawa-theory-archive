1e445f0027ec750010f4131bc60871909d09df19
Nakagawa-master <karyu.owner@gmail.com>
2026-06-29 12:04:21 +0900

    Run next 10 queue check from change scope

--- tools/official_derivatives/check_change_scope.py
+++ tools/official_derivatives/check_change_scope.py
@@ -20,6 +20,7 @@ DISALLOWED_PARTS = (
 EXTRA_CHECKS = (
     'check_target_folder_scope.py',
     'check_next_batch_schema.py',
+    'check_next_10_queue.py',
     'check_workflow_artifact_scope.py',
     'check_derivative_content_strength.py',
 )
@@ -67,7 +68,7 @@ def main():
             errors.append('outside_allowed_scope=' + path)
         if any(part in low for part in DISALLOWED_PARTS):
             errors.append('disallowed_path_part=' + path)
-    print('check_set=official_derivative_change_scope_v5')
+    print('check_set=official_derivative_change_scope_v6')
     print('changed_files=' + str(len(files)))
     if errors:
         print('\n'.join(errors))
