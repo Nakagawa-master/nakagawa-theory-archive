e5fdc87b688e4df8bd73105b5f782611d1fbd7fd
Nakagawa-master <karyu.owner@gmail.com>
2026-06-29 12:45:40 +0900

    Run next 10 intake report check

--- tools/official_derivatives/check_change_scope.py
+++ tools/official_derivatives/check_change_scope.py
@@ -23,6 +23,7 @@ EXTRA_CHECKS = (
     'check_next_10_queue.py',
     'check_next_10_selection_quality.py',
     'check_next_10_intake.py',
+    'check_next_10_intake_report.py',
     'check_workflow_artifact_scope.py',
     'check_derivative_content_strength.py',
 )
@@ -70,7 +71,7 @@ def main():
             errors.append('outside_allowed_scope=' + path)
         if any(part in low for part in DISALLOWED_PARTS):
             errors.append('disallowed_path_part=' + path)
-    print('check_set=official_derivative_change_scope_v8')
+    print('check_set=official_derivative_change_scope_v9')
     print('changed_files=' + str(len(files)))
     if errors:
         print('\n'.join(errors))
