141145e93ac6fe98dd64fdde1be6b5f40ce0813d
Nakagawa-master <karyu.owner@gmail.com>
2026-06-29 13:31:39 +0900

    Run source inventory validator in change scope check

--- tools/official_derivatives/check_change_scope.py
+++ tools/official_derivatives/check_change_scope.py
@@ -21,6 +21,7 @@ EXTRA_CHECKS = (
     'check_target_folder_scope.py',
     'check_next_batch_schema.py',
     'check_next_10_queue.py',
+    'check_next_10_source_inventory.py',
     'check_next_10_source_candidate_schema.py',
     'check_next_10_source_quality_gate.py',
     'check_next_10_source_candidate_report.py',
@@ -74,7 +75,7 @@ def main():
             errors.append('outside_allowed_scope=' + path)
         if any(part in low for part in DISALLOWED_PARTS):
             errors.append('disallowed_path_part=' + path)
-    print('check_set=official_derivative_change_scope_v11')
+    print('check_set=official_derivative_change_scope_v12')
     print('changed_files=' + str(len(files)))
     if errors:
         print('\n'.join(errors))
