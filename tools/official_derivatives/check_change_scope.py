861c40d14f453763bc5447bd825e74d1dffd0644
Nakagawa-master <karyu.owner@gmail.com>
2026-06-29 12:37:28 +0900

    Run next 10 selection quality check

--- tools/official_derivatives/check_change_scope.py
+++ tools/official_derivatives/check_change_scope.py
@@ -21,6 +21,7 @@ EXTRA_CHECKS = (
     'check_target_folder_scope.py',
     'check_next_batch_schema.py',
     'check_next_10_queue.py',
+    'check_next_10_selection_quality.py',
     'check_workflow_artifact_scope.py',
     'check_derivative_content_strength.py',
 )
@@ -68,7 +69,7 @@ def main():
             errors.append('outside_allowed_scope=' + path)
         if any(part in low for part in DISALLOWED_PARTS):
             errors.append('disallowed_path_part=' + path)
-    print('check_set=official_derivative_change_scope_v6')
+    print('check_set=official_derivative_change_scope_v7')
     print('changed_files=' + str(len(files)))
     if errors:
         print('\n'.join(errors))
