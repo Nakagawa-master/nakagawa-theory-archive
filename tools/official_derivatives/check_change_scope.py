8ab2d512e56afd0f5400b456c3190153cc64a8d7
Nakagawa-master <karyu.owner@gmail.com>
2026-06-29 13:20:56 +0900

    Run source candidate report validator in change scope check

--- tools/official_derivatives/check_change_scope.py
+++ tools/official_derivatives/check_change_scope.py
@@ -23,6 +23,7 @@ EXTRA_CHECKS = (
     'check_next_10_queue.py',
     'check_next_10_source_candidate_schema.py',
     'check_next_10_source_quality_gate.py',
+    'check_next_10_source_candidate_report.py',
     'check_next_10_selection_quality.py',
     'check_next_10_intake.py',
     'check_next_10_intake_report.py',
@@ -73,7 +74,7 @@ def main():
             errors.append('outside_allowed_scope=' + path)
         if any(part in low for part in DISALLOWED_PARTS):
             errors.append('disallowed_path_part=' + path)
-    print('check_set=official_derivative_change_scope_v10')
+    print('check_set=official_derivative_change_scope_v11')
     print('changed_files=' + str(len(files)))
     if errors:
         print('\n'.join(errors))
