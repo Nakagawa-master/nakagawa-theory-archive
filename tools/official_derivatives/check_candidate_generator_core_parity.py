e55e765a7eca0a8b5a89142eff98de8698134c4e
Nakagawa-master <karyu.owner@gmail.com>
2026-06-30 11:22:26 +0900

    Check target adapter in generator parity gate

--- tools/official_derivatives/check_candidate_generator_core_parity.py
+++ tools/official_derivatives/check_candidate_generator_core_parity.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import six_page_template_core as core
 import universal_page_renderer as universal
+from official_derivative_target_adapter import load_universal_candidate_05_09
 
 GENERATOR = Path(__file__).resolve().with_name('generate_candidate_05_09_html.py')
 spec = importlib.util.spec_from_file_location('candidate_generator', GENERATOR)
@@ -18,6 +19,9 @@ if mod.SHARED_STYLE != core.SHARED_STYLE:
     errors.append('shared_style_mismatch')
 if list(mod.CHILD_CARDS) != list(core.CHILD_CARDS):
     errors.append('child_cards_mismatch')
+adapter_rows = load_universal_candidate_05_09()
+if len(adapter_rows) != len(mod.TARGETS):
+    errors.append('adapter_count_mismatch')
 checked = 0
 for record in mod.TARGETS:
     current_pages = mod.pages_for(record)
@@ -27,9 +31,10 @@ for record in mod.TARGETS:
         if current_pages.get(rel) != universal_pages.get(rel):
             errors.append('render_mismatch=' + record['folder'] + ':' + rel)
 
-print('check_set=candidate_generator_core_parity_v2')
+print('check_set=candidate_generator_core_parity_v3')
 print('pages=' + str(len(core.PAGES)))
 print('child_cards=' + str(len(core.CHILD_CARDS)))
+print('adapter_rows=' + str(len(adapter_rows)))
 print('render_checked=' + str(checked))
 if errors:
     print('\n'.join(errors[:40]))
