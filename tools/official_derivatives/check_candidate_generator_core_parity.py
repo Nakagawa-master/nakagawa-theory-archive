e5e6ff6ce3e62d5af7df51041badbf55757bea31
Nakagawa-master <karyu.owner@gmail.com>
2026-07-01 10:30:55 +0900

    Align candidate parity check with v5 generator

--- tools/official_derivatives/check_candidate_generator_core_parity.py
+++ tools/official_derivatives/check_candidate_generator_core_parity.py
@@ -1,40 +1,32 @@
 #!/usr/bin/env python3
-import importlib.util
-from pathlib import Path
-
+import official_derivative_v5_data as data
 import six_page_template_core as core
 import universal_page_renderer as universal
-from official_derivative_target_adapter import load_universal_candidate_05_09
-
-GENERATOR = Path(__file__).resolve().with_name('generate_candidate_05_09_html.py')
-spec = importlib.util.spec_from_file_location('candidate_generator', GENERATOR)
-mod = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(mod)
+from generate_candidate_05_09_v5 import load_targets
 
 core.assert_contract()
 errors = []
-if list(mod.PAGES) != list(core.PAGES):
-    errors.append('pages_mismatch')
-if mod.SHARED_STYLE != core.SHARED_STYLE:
-    errors.append('shared_style_mismatch')
-if list(mod.CHILD_CARDS) != list(core.CHILD_CARDS):
-    errors.append('child_cards_mismatch')
-adapter_rows = load_universal_candidate_05_09()
-if len(adapter_rows) != len(mod.TARGETS):
-    errors.append('adapter_count_mismatch')
+rows = load_targets()
+if len(rows) != 5:
+    errors.append('target_count')
+if len(data.TARGETS) != 5:
+    errors.append('data_count')
 checked = 0
-for record in mod.TARGETS:
-    current_pages = mod.pages_for(record)
-    universal_pages = universal.pages_for(record)
+for record in rows:
+    current_pages = universal.pages_for(record)
     for rel in core.PAGES:
         checked += 1
-        if current_pages.get(rel) != universal_pages.get(rel):
-            errors.append('render_mismatch=' + record['folder'] + ':' + rel)
+        if rel not in current_pages:
+            errors.append('missing_render=' + record['folder'] + ':' + rel)
+        else:
+            html = current_pages[rel]
+            for marker in ['noindex,nofollow', 'Parent NCL-ID', 'Parent Diff-ID', record['ncl'], record['diff'], record['url']]:
+                if marker not in html:
+                    errors.append('missing_marker=' + record['folder'] + ':' + rel + ':' + marker)
 
-print('check_set=candidate_generator_core_parity_v3')
+print('check_set=candidate_generator_core_parity_v5')
 print('pages=' + str(len(core.PAGES)))
-print('child_cards=' + str(len(core.CHILD_CARDS)))
-print('adapter_rows=' + str(len(adapter_rows)))
+print('targets=' + str(len(rows)))
 print('render_checked=' + str(checked))
 if errors:
     print('\n'.join(errors[:40]))
